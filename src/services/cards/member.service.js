ae363e569bb04776d1c83e8b326b4d539e290e49
GitTuanKiet <tuank1792@gmail.com>
2024-02-21 21:51:35 +0700

    fix:add-memberCardId-when-registerMemberCard

--- src/services/cards/member.service.js
+++ src/services/cards/member.service.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-catch */
 import { MemberCardModels } from 'models/cards/member.model'
 import { UserModels } from 'models/user.model'
+import { fixObjectId } from 'utils/formatters'
 
 const InvalidFields = ['_id', 'userId', 'createdAt', 'updatedAt']
 
@@ -21,9 +22,13 @@ const registerMemberCard = async (userId, data) => {
     if (points >= 10000) level = 'gold'
     if (points >= 50000) level = 'platinum'
 
+    // lấy _id từ inserted
     const newMemberCard = { userId: userId, ...data, level }
+    const inserted = await MemberCardModels.createMemberCard(newMemberCard)
 
-    return await MemberCardModels.createMemberCard(newMemberCard)
+    // update user memberCardId
+    await UserModels.updateUser(userId, { memberCardId: fixObjectId(inserted._id) })
+    return inserted
   } catch (error) {
     throw error
   }
